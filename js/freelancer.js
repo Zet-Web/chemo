d8218e8a81e4378bb16e1ee1698cce5f9b70c925
Zet-Web <zet.pick@gmail.com>
2018-05-10 17:54:06 +0500

    Full  main page without modal
    Signed-off-by: Zet-Web <zet.pick@gmail.com>

--- js/freelancer.js
+++ js/freelancer.js
@@ -105,9 +105,9 @@ jQuery(document).ready(function ($) {
         },
         $ThumbnailNavigatorOptions: {
             $Class: $JssorThumbnailNavigator$,
-            $Rows: 2,
-            $SpacingX: 14,
-            $SpacingY: 12,
+            $Rows: 1,
+            $SpacingX: 12,
+            $SpacingY: 10,
             $Orientation: 2,
             $Align: 156
         }
@@ -115,19 +115,75 @@ jQuery(document).ready(function ($) {
 
     var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
 
+    /* ОТЗЫВЫ СЛАЙДЕР */
+
+    var jssor_2_SlideshowTransitions = [
+        {$Duration:1200,$Zoom:1,$Easing:{$Zoom:$Jease$.$InCubic,$Opacity:$Jease$.$OutQuad},$Opacity:2},
+        {$Duration:1000,$Zoom:11,$SlideOut:true,$Easing:{$Zoom:$Jease$.$InExpo,$Opacity:$Jease$.$Linear},$Opacity:2},
+        {$Duration:1200,$Zoom:1,$Rotate:1,$During:{$Zoom:[0.2,0.8],$Rotate:[0.2,0.8]},$Easing:{$Zoom:$Jease$.$Swing,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$Swing},$Opacity:2,$Round:{$Rotate:0.5}},
+        {$Duration:1000,$Zoom:11,$Rotate:1,$SlideOut:true,$Easing:{$Zoom:$Jease$.$InQuint,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$InQuint},$Opacity:2,$Round:{$Rotate:0.8}},
+        {$Duration:1200,x:0.5,$Cols:2,$Zoom:1,$Assembly:2049,$ChessMode:{$Column:15},$Easing:{$Left:$Jease$.$InCubic,$Zoom:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+        {$Duration:1200,x:4,$Cols:2,$Zoom:11,$SlideOut:true,$Assembly:2049,$ChessMode:{$Column:15},$Easing:{$Left:$Jease$.$InExpo,$Zoom:$Jease$.$InExpo,$Opacity:$Jease$.$Linear},$Opacity:2},
+        {$Duration:1200,x:0.6,$Zoom:1,$Rotate:1,$During:{$Left:[0.2,0.8],$Zoom:[0.2,0.8],$Rotate:[0.2,0.8]},$Opacity:2,$Round:{$Rotate:0.5}},
+        {$Duration:1000,x:-4,$Zoom:11,$Rotate:1,$SlideOut:true,$Easing:{$Left:$Jease$.$InQuint,$Zoom:$Jease$.$InQuart,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$InQuint},$Opacity:2,$Round:{$Rotate:0.8}},
+        {$Duration:1200,x:-0.6,$Zoom:1,$Rotate:1,$During:{$Left:[0.2,0.8],$Zoom:[0.2,0.8],$Rotate:[0.2,0.8]},$Opacity:2,$Round:{$Rotate:0.5}},
+        {$Duration:1000,x:4,$Zoom:11,$Rotate:1,$SlideOut:true,$Easing:{$Left:$Jease$.$InQuint,$Zoom:$Jease$.$InQuart,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$InQuint},$Opacity:2,$Round:{$Rotate:0.8}},
+        {$Duration:1200,x:0.5,y:0.3,$Cols:2,$Zoom:1,$Rotate:1,$Assembly:2049,$ChessMode:{$Column:15},$Easing:{$Left:$Jease$.$InCubic,$Top:$Jease$.$InCubic,$Zoom:$Jease$.$InCubic,$Opacity:$Jease$.$OutQuad,$Rotate:$Jease$.$InCubic},$Opacity:2,$Round:{$Rotate:0.7}},
+        {$Duration:1000,x:0.5,y:0.3,$Cols:2,$Zoom:1,$Rotate:1,$SlideOut:true,$Assembly:2049,$ChessMode:{$Column:15},$Easing:{$Left:$Jease$.$InExpo,$Top:$Jease$.$InExpo,$Zoom:$Jease$.$InExpo,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$InExpo},$Opacity:2,$Round:{$Rotate:0.7}},
+        {$Duration:1200,x:-4,y:2,$Rows:2,$Zoom:11,$Rotate:1,$Assembly:2049,$ChessMode:{$Row:28},$Easing:{$Left:$Jease$.$InCubic,$Top:$Jease$.$InCubic,$Zoom:$Jease$.$InCubic,$Opacity:$Jease$.$OutQuad,$Rotate:$Jease$.$InCubic},$Opacity:2,$Round:{$Rotate:0.7}},
+        {$Duration:1200,x:1,y:2,$Cols:2,$Zoom:11,$Rotate:1,$Assembly:2049,$ChessMode:{$Column:19},$Easing:{$Left:$Jease$.$InCubic,$Top:$Jease$.$InCubic,$Zoom:$Jease$.$InCubic,$Opacity:$Jease$.$OutQuad,$Rotate:$Jease$.$InCubic},$Opacity:2,$Round:{$Rotate:0.8}}
+    ];
+
+    var jssor_2_options = {
+        $AutoPlay: 1,
+        $SlideshowOptions: {
+            $Class: $JssorSlideshowRunner$,
+            $Transitions: jssor_2_SlideshowTransitions,
+            $TransitionsOrder: 1
+        },
+        $ArrowNavigatorOptions: {
+            $Class: $JssorArrowNavigator$
+        },
+        $ThumbnailNavigatorOptions: {
+            $Class: $JssorThumbnailNavigator$,
+            $Rows: 1,
+            $SpacingX: 12,
+            $SpacingY: 10,
+            $Orientation: 2,
+            $Align: 156
+        }
+    };
+
+    var jssor_2_slider = new $JssorSlider$("jssor_2", jssor_2_options);
+
   /*#region responsive code begin*/
 
-    var MAX_WIDTH = 960;
+    var MAX_WIDTH = 1120;
 
-    function ScaleSlider() {
-        var containerElement = jssor_1_slider.$Elmt.parentNode;
+    function ScaleSlider_2() {
+        var containerElement = jssor_2_slider.$Elmt.parentNode;
         var containerWidth = containerElement.clientWidth;
 
         if (containerWidth) {
 
             var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
 
-            jssor_1_slider.$ScaleWidth(expectedWidth);
+            jssor_2_slider.$ScaleWidth(expectedWidth);
+        }
+        else {
+            window.setTimeout(ScaleSlider, 30);
+        }
+    }
+
+    function ScaleSlider() {
+        var containerElement = jssor_2_slider.$Elmt.parentNode;
+        var containerWidth = containerElement.clientWidth;
+
+        if (containerWidth) {
+
+            var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+
+            jssor_2_slider.$ScaleWidth(expectedWidth);
         }
         else {
             window.setTimeout(ScaleSlider, 30);
@@ -135,9 +191,123 @@ jQuery(document).ready(function ($) {
     }
 
     ScaleSlider();
+    ScaleSlider_2();
 
     $(window).bind("load", ScaleSlider);
     $(window).bind("resize", ScaleSlider);
     $(window).bind("orientationchange", ScaleSlider);
   /*#endregion responsive code end*/
+
+
+  /* ДО КОНЦА АКЦИИ ОСТАЛОСь */
+
+    var deadline = addDaysToDate(new Date(), 3).getTime();
+    var daysSpan = document.getElementById('days');
+    var hoursSpan = document.getElementById('hours');
+    var minutesSpan = document.getElementById('minutes');
+    var secondsSpan = document.getElementById('seconds');
+
+    updateClock(deadline);
+    var interval = setInterval(updateClock, 1000);
+
+    function addDaysToDate(startDate, numberOfDays) {
+        return new Date(
+            startDate.getFullYear(),
+            startDate.getMonth(),
+            startDate.getDate() + numberOfDays,
+            startDate.getHours(),
+            startDate.getMinutes(),
+            startDate.getSeconds()
+        );
+    }
+
+    function getRemainingTime(deadline) {
+        var total = deadline - new Date().getTime();
+
+        if (isNaN(total)) {
+            return false;
+        }
+
+        var seconds = Math.floor( (total / 1000) % 60 );
+        var minutes = Math.floor( (total / 1000 / 60) % 60 );
+        var hours = Math.floor( (total / (1000 * 60 * 60)) % 24 );
+        var days = Math.floor( total / (1000 * 60 * 60 * 24) );
+
+        return {
+            'total': total,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function updateClock() {
+        var remainingTime = getRemainingTime(deadline);
+
+        if (remainingTime.total <= 0) {
+            clearInterval(interval);
+
+            document.getElementById('expired').classList.add('show');
+
+            return false;
+        } else if (!remainingTime) {
+            return false;
+        }
+
+        daysSpan.innerText = addLeadingZeros(remainingTime.days);
+        hoursSpan.innerText = addLeadingZeros(remainingTime.hours);
+        minutesSpan.innerText = addLeadingZeros(remainingTime.minutes);
+        secondsSpan.innerText = addLeadingZeros(remainingTime.seconds);
+    }
+
+    function addLeadingZeros(time) {
+        return ('0' + time).slice(-2);
+    }
+
+
+
+    /* GALLERY LIGHTBOX - ГАЛЕРЕЯ ТОВАРОВ*/
+
+    $(document).on("click", '[data-toggle="lightbox"]', function(event) {
+        event.preventDefault();
+        $(this).ekkoLightbox();
+    });
+
+
+    /* GOOGLE MAP INIT ИНИТ ГУГЛ КАРТЫ */
+    function initialize() {
+        var myLatlng = new google.maps.LatLng(55.811299, 37.835151),
+            mapOptions = {
+                zoom: 13,
+                center: myLatlng,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            }
+        var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        var contentString = 'Times Square, Manhattan';
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth: 500
+        });
+
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map,marker);
+        });
+
+        google.maps.event.addDomListener(window, "resize", function() {
+            var center = map.getCenter();
+            google.maps.event.trigger(map, "resize");
+            map.setCenter(center);
+        });
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+
 });
+
+
