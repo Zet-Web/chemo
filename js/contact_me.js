0dd618e20559ae44b32dff496f66dece15297419
Zet-Web <zet.pick@gmail.com>
2018-05-16 12:36:53 +0500

    pre-release chemodan-msk Signed-off-by: Zet-Web <zet.pick@gmail.com>

--- js/contact_me.js
+++ js/contact_me.js
@@ -35,7 +35,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>Спасибо за обращение, ваше сообщение было успешно отправлено. Наши менеджеры свяжутся с вами в ближайшее время. </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
